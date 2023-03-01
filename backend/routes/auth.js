aac9fcdb59c007a5f4569511fc5ef5e096fe21b4
Ghazanfar Ali <gak_92@hotmail.com>
2023-03-01 13:38:16 +0500

    Saving register user data to the online mongo database using promises

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,52 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+
+require("../db/conn");
+const User = require("../models/userSchema");
 
 router.get("/", (req, res) => {
   res.send(`Hello World from the auth.js`);
 });
 
-router.post('/register', (req, res) => {
-  console.log(req.body);
-  res.json({
-    message: req.body,
-  });
+router.post("/register", (req, res) => {
+  const { name, email, phone, work, password, cpassword } = req.body;
+
+  if (!name || !email || !phone || !work || !password || !cpassword) {
+    return res.status(422).json({
+      error: "please fill all fields",
+    });
+  }
+
+  User.findOne({ email:email })
+    .then((userExist) => {
+      if (userExist) {
+        return res.status(422).json({
+          error: "User already exist",
+        });
+      }
+
+      const user = new User({ name, email, phone, work, password, cpassword });
+
+      user
+        .save()
+        .then(() => {
+          res.status(201).json({
+            message: "User Registered successfully",
+          });
+        })
+        .catch((err) =>
+          res.status(500).json({
+            error: "Failed to register",
+          })
+        );
+    })
+    .catch((err) => console.log(err));
+
+  // console.log(name);
+  // console.log(email);
+  // res.json({
+  //   message: req.body,
+  // });
 });
 
 module.exports = router;
