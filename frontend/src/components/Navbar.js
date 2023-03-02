101279439a284409f8d6466ff1106b33fd7e7e69
Ghazanfar Ali <gak_92@hotmail.com>
2023-03-02 12:33:53 +0500

    Create signup page

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <Link className="navbar-brand" to="/">
-          Navbar
+          Logo
         </Link>
         <button
           className="navbar-toggler"
--- frontend/src/components/Registration.js
+++ frontend/src/components/Registration.js
@@ -1,8 +1,64 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import signpic from '../images/signup.jpg';
 
 const Registration = () => {
   return (
-    <div>Registration</div>
+    <>
+      <section className='signup'>
+        <div className='container mt-5'>
+          <div className='signup-content'>
+            <div className='signup-form'>
+              <h2 className='form-title'>Sing Up</h2>
+              <form className='register-form' id='register-form'>
+                <div className='form-group'>
+                  <label htmlFor='name'><i class="zmdi zmdi-account material-icons-name"></i></label>
+                  <input type='text' name='name' id='name' autoComplete='off' placeholder='Your Name' />
+                </div>
+
+                <div className='form-group'>
+                  <label htmlFor='email'><i class="zmdi zmdi-email material-icons-name"></i></label>
+                  <input type='email' name='email' id='email' autoComplete='off' placeholder='Your Email' />
+                </div>
+
+                <div className='form-group'>
+                  <label htmlFor='phone'><i class="zmdi zmdi-phone-in-talk material-icons-name"></i></label>
+                  <input type='number' name='phone' id='phone' autoComplete='off' placeholder='Your Mobile Number' />
+                </div>
+
+                <div className='form-group'>
+                  <label htmlFor='work'><i class="zmdi zmdi-slideshow material-icons-name"></i></label>
+                  <input type='text' name='work' id='work' autoComplete='off' placeholder='Your Profession' />
+                </div>
+
+                <div className='form-group'>
+                  <label htmlFor='password'><i class="zmdi zmdi-lock material-icons-name"></i></label>
+                  <input type='password' name='password' id='password' autoComplete='off' placeholder='Your Password' />
+                </div>
+
+                <div className='form-group'>
+                  <label htmlFor='cpassword'><i class="zmdi zmdi-lock material-icons-name"></i></label>
+                  <input type='password' name='cpassword' id='cpassword' autoComplete='off' placeholder='Confirm our Password' />
+                </div>
+
+                <div className='form-group form-button'>
+                  <input type='submit' name='signup' id='signup' className='form-submit' value='Register' />
+                </div>
+
+              </form>
+            </div>
+
+            <div className='signup-image'>
+              <figure>
+                <img src={signpic} alt='Registration pic' />
+              </figure>
+              <NavLink to='/login' className='signup-image-link'>Already Registter</NavLink>
+            </div>
+          </div>
+        </div>
+
+      </section>
+    </>
   )
 }
 
