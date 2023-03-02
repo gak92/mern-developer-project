c718a091f85679a4811d572efc8999c51db214d7
Ghazanfar Ali <gak_92@hotmail.com>
2023-03-02 13:08:51 +0500

    Create contact form

--- frontend/src/components/Contact.js
+++ frontend/src/components/Contact.js
@@ -2,7 +2,37 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+    
+      {/* Cntactt Frm */}
+      <div className='contact_form'>
+        <div className='container'>
+          <div className='row'>
+            <div className='col-lg-10 offset-lg-1'>
+              <div className='contact_form_container py-5'>
+                <h2 className='contact_form_title'>Get In Touch</h2>
+                <form id='contact_form'>
+                  <div className='contact_form_name d-flex justify-content-between aligns-item-centre'>
+                    <input type='text' id='cf_name' className='cf_name input_field' placeholder='Your Name' required='true' />
+                    <input type='email' id='cf_email' className='cf_email input_field' placeholder='Your Email' required='true' />
+                    <input type='number' id='cf_number' className='cf_number input_field' placeholder='Your Number' required='true' />
+
+                  </div>
+
+                  <div className='cf_text mt-5'>
+                    <textarea className='cf_message text_field' placeholder='Message' cols='90' rows='10'/>
+                  </div>
+
+                  <div className='cf_button'>
+                    <button type='submit' className='btn btn-primary'>Send Message</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
