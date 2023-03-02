3ad5c3a73273f343e208054b1d1b2692f5b360bf
Ghazanfar Ali <gak_92@hotmail.com>
2023-03-02 13:16:41 +0500

    Create home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-      <p>Welcome</p>
-      <h1>We are the MERN Developers</h1>
+    <div className='d-flex flex-column justify-content-center align-items-center pt-5 mt-5'>
+      <div>Welcome</div>
+      <br />
+      <div><h1>We are the MERN Developers</h1></div>
     </div>
   )
 }
