aec596ce282d0d678132cec9f129a53cca676c4a
Ghazanfar Ali <gak_92@hotmail.com>
2023-03-02 12:43:31 +0500

    Create login page

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,8 +1,68 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+      <section className="signup">
+        <div className="container mt-5">
+          <div className="signup-content">
+
+          <div className="signup-image">
+              {/* <figure>
+                <img src={signpic} alt="Registration pic" />
+              </figure> */}
+              <NavLink to="/registration" className="signup-image-link">
+                Register
+              </NavLink>
+            </div>
+
+            <div className="signup-form">
+              <h2 className="form-title">Log In</h2>
+              <form className="login-form" id="login-form">
+                
+                <div className="form-group">
+                  <label htmlFor="email">
+                    <i class="zmdi zmdi-email material-icons-name"></i>
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    autoComplete="off"
+                    placeholder="Your Email"
+                  />
+                </div>
+
+                <div className="form-group">
+                  <label htmlFor="password">
+                    <i class="zmdi zmdi-lock material-icons-name"></i>
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    autoComplete="off"
+                    placeholder="Your Password"
+                  />
+                </div>
+
+                <div className="form-group form-button">
+                  <input
+                    type="submit"
+                    name="signin"
+                    id="signin"
+                    className="form-submit"
+                    value="Log In"
+                  />
+                </div>
+              </form>
+            </div>
+
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
